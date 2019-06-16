510109eb40c4bb1dbc1efacde43386c615390966
JaydipMakwana <jaydip.makwana@botreetechnologies.com>
2019-06-16 21:39:27 +0530

    added sorting, searching

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function readFormData(argument) {
 }
 
 function insertNewRecord(data) {
-  var table = document.getElementById('employee_list').getElementsByTagName('tbody')[0];
-  var newRow = table.insertRow(table.length);
+  var table_body = document.getElementById('employeeListBody');
+  var newRow = table_body.insertRow(table_body.length);
 
   cell1 = newRow.insertCell(0);
   cell1.innerHTML = data.fullname;
@@ -44,6 +44,22 @@ function resetForm() {
   document.getElementById('emp_code').value = "";
   document.getElementById('salary').value = "";
   document.getElementById('city').value = "";
+  var searchInput = document.getElementById('searchInput');
+  searchInput.value = "";
+  var event = document.createEvent('KeyboardEvent'); // create a key event
+  event.initKeyEvent("keydown", // typeArg,
+                      true,             // canBubbleArg,
+                      true,             // cancelableArg,
+                      null,             // viewArg,  Specifies UIEvent.view. This value may be null.
+                      false,            // ctrlKeyArg,
+                      false,            // altKeyArg,
+                      false,            // shiftKeyArg,
+                      false,            // metaKeyArg,
+                      13,               // keyCodeArg,
+                      0);               // charCodeArg);
+
+  searchInput.dispatchEvent(event);
+
   selectedRow = null;
 }
 
@@ -66,7 +82,7 @@ function updateRecord(formData) {
 function onDelete(td) {
   if(confirm('Are you sure to delete this record ?')){
     row = td.parentElement.parentElement;
-    document.getElementById('employee_list').deleteRow(row.rowIndex);
+    document.getElementById('employeeList').deleteRow(row.rowIndex);
     resetForm();
   }
 }
@@ -83,7 +99,95 @@ function validate() {
       document.getElementById('fullnameValidation').classList.add('hide');
     }
   }
-
   return isValid;
-
 }
+
+function sortingTable(event){
+  var table_body = document.getElementById('employeeListBody');
+  var items = table_body.childNodes;
+  var check_column = ['sortByFullName', 'sortByEmpCode', 'sortBySalary', 'sortByCity'];
+  var itemsArr = [];
+  for (var i in items) {
+    if (items[i].nodeType == 1) {
+      itemsArr.push(items[i]);
+    }
+  }
+
+  itemsArr.sort(function (a, b) {
+    var find_index = check_column.indexOf(event.target.id)
+    var a_childNode = a.childNodes[find_index].innerHTML.toLowerCase();
+    var b_childNode = b.childNodes[find_index].innerHTML.toLowerCase();
+    if (find_index == 1 || find_index == 2){
+      return parseInt(a_childNode) == parseInt(b_childNode)
+        ? 0
+        : (parseInt(a_childNode) > parseInt(b_childNode) ? 1 : -1);
+    }else{
+      return a_childNode == b_childNode
+        ? 0
+        : (a_childNode > b_childNode ? 1 : -1);
+    }
+  });
+
+  for (i = 0; i < itemsArr.length; ++i) {
+    table_body.appendChild(itemsArr[i]);
+  }
+}
+
+function searchFromTable(event) {
+  if(event.keyCode == 13){
+    var table_body = document.getElementById('employeeListBody');
+    var value = event.target.value.toLowerCase();
+    var tr = table_body.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toLowerCase().indexOf(value) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+}
+
+
+// For sorting
+
+// function sortTable(n) {
+//   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+//   table = document.getElementById("employeeListBody");
+//   switching = true;
+//   dir = "asc";
+//   while (switching) {
+//     switching = false;
+//     rows = table.rows;
+//     for (i = 0; i < (rows.length - 1); i++) {
+//       shouldSwitch = false;
+//       x = rows[i].getElementsByTagName("TD")[n];
+//       y = rows[i + 1].getElementsByTagName("TD")[n];
+//       if (dir == "asc") {
+//         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+//           shouldSwitch = true;
+//           break;
+//         }
+//       } else if (dir == "desc") {
+//         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+//           shouldSwitch = true;
+//           break;
+//         }
+//       }
+//     }
+//     if (shouldSwitch) {
+//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//       switching = true;
+//       switchcount++;
+//     } else {
+//       if (switchcount == 0 && dir == "asc") {
+//         dir = "desc";
+//         switching = true;
+//       }
+//     }
+//   }
+// }
